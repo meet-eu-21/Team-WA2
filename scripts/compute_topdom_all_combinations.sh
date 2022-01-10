58a7947dc9d9609d6496a6b1d516ba55b863177d
AniaSemik <annaw.semik@gmail.com>
2022-01-10 18:46:30 +0100

    Scores of all outputs

--- scripts/compute_topdom_all_combinations.sh
+++ scripts/compute_topdom_all_combinations.sh
@@ -1,7 +1,7 @@
-peak_find_funk=("None" "find_min" "detect_local_extrema" "find_peaks" "find_peaks_2")
+peak_find_funk=("find_min" "detect_local_extrema" "find_peaks" "find_peaks_2")
 smooth_func=("False" "savgol_filter" "smooth" "qspline")
 filter_func=("False" "statFilter" "filter_peaks")
-chromosomes=($(seq 1 2))
+chromosomes=($(seq 1 22))
 chromosomes+=("X")
 
 echo "start"
@@ -17,16 +17,17 @@ do
         for f in "${filter_func[@]}";
         do
             filterfunc="-f $f"
-            outfile="-o ${p}_${s}_${f}"
+            outfile="${p}_${s}_${f}"
             for i in "${chromosomes[@]}";
             do
                 basic_command="python3 TopDom.py -i /home/eu_plus_3/Team-WA2/normalisation/NORMALnormalisation/normalised_matrices/contact_map_chr$i.tsv -r 100k -d ./combination_outputs/"
                 echo "Topdom for chromosome $i contact map."
                 echo "Chromosome $i $outfile"
-                eval "$basic_command $peakfindfunc $smoothfunc $filterfunc $outfile"
+                eval "$basic_command $peakfindfunc $smoothfunc $filterfunc -o $outfile"
                 echo "Chromosome $i done."
             done
-            cat -n for_overlap_scores__${outfile}__chr*.csv | sort -uk2 | sort -nk1 | cut -f2- > for_overlap_scores__${outfile}__allchr.csv
+            eval "cat ./combination_outputs/for_overlap_scores__${outfile}__chr*.csv >> ./combination_outputs/for_overlap_scores__${outfile}__all.csv"
+            eval "cat -n ./combination_outputs/for_overlap_scores__${outfile}__all.csv | sort -uk2 | sort -nk1 | cut -f2- > ./combination_outputs/for_overlap_scores__${outfile}__allchr.csv"
         done
     done
 done
